615e559201fe1fa806ef7a61f6ad93dea92072fa
xmduhan <xmduhan@gmail.com>
2013-12-08 10:52:12 +0800

    ...

--- mt4lib/config.py
+++ mt4lib/config.py
@@ -1,24 +1,18 @@
 # -*- coding:utf-8 -*- 
 
-def read(file,var):
+def ReadValue(file,var):
     try:
-        lc = {}
-        execfile(file,lc)
-        config = lc["config"]
-        code = "config['%s']" % "']['".join(var.split("\\"))
+        g = {}
+        execfile(file,g)
+        code = "g['%s']" % "']['".join(var.split("/"))
         result = eval(code)  
     except:
         result = None
     return result
 
-
-def test(file,var):   
-    return file
-
-
 ''' a code for testing 
 file = "I:\\Program Files\\HotForex MetaTrader\\experts\\config\\pycfg\\Scalping.py"
-var = "HF Markets Ltd\\Micro\\EURCAD\\bands_period"
+var = "HF Markets Ltd/Micro/EURCAD/bands_period"
 value = read(file,var)
 print(value)
 '''
