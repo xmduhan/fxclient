aeb8a52692b98b3439b06a851570719f646c1c2e
xmduhan <xmduhan@gmail.com>
2013-12-08 22:49:36 +0800

    ...

--- mt4lib/config.py
+++ mt4lib/config.py
@@ -1,6 +1,46 @@
 # -*- coding:utf-8 -*- 
 
-def ReadValue(file,var):
+def readFile(file):
+    try:
+        g = {}
+        execfile(file,g)        
+        result = unicode(g["config"])
+    except:
+        result = unicode({})
+    return result
+
+
+def printTrue(step):
+    try:
+        print(step,"True=",True)
+    except:
+        print(step,"True=",None)
+
+def readDictValue(dictStr,path):
+    try:        
+        config = eval(dictStr)
+        result = eval("config['%s']" % "']['".join(path.split("/")))
+    except Exception as e:
+        result = None
+    return result
+
+def readDictValueStr(dictStr,path):
+    try:
+        result = unicode(readDictValue(dictStr,path))
+    except:
+        result = "None"
+    return result
+          
+def readDictValueType(dictStr,path):
+    try:        
+        result = unicode(type(readDictValue(dictStr,path)))
+    except:
+        result = "NoneType"
+    return result
+
+
+'''
+def ReadFile(file):
     try:
         g = {}
         execfile(file,g)
@@ -9,6 +49,8 @@ def ReadValue(file,var):
     except:
         result = None
     return result
+'''
+
 
 ''' a code for testing 
 file = "I:\\Program Files\\HotForex MetaTrader\\experts\\config\\pycfg\\Scalping.py"
---
+++ mt4lib/service.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*- 
+
+import urllib,urllib2,cookielib,json
+
+#ckjar = cookielib.CookieJar()
+#ckproc = urllib2.HTTPCookieProcessor(ckjar)
+#opener = urllib2.build_opener(ckproc)
+#urllib2.install_opener(opener)
+
+def expertRegister(expertCode,loginId,companyName,serverName):
+    data = {
+        "ExpertCode":expertCode,
+        "AccountLoginId":loginId,
+        "AccountCompanyName":companyName,
+        "AccountServerName":serverName
+    }
+    postData = urllib.urlencode(data)
+    url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertRegister"
+    #url = "http://192.168.0.120:8080/experts/service/ExpertRegister"
+    req = urllib2.Request(url, postData)
+    response = urllib2.urlopen(req)
+    content = response.read()
+    return  str(json.loads(content))
+    
+
+
+
+        
+        
