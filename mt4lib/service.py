c9b6eb4ac7fd3d41e62eec56586b4873716357dd
xmduhan <xmduhan@gmail.com>
2013-12-09 23:14:36 +0800

    ...

--- mt4lib/service.py
+++ mt4lib/service.py
@@ -8,19 +8,23 @@ import urllib,urllib2,cookielib,json
 #urllib2.install_opener(opener)
 
 def expertRegister(expertCode,loginId,companyName,serverName):
-    data = {
-        "ExpertCode":expertCode,
-        "AccountLoginId":loginId,
-        "AccountCompanyName":companyName,
-        "AccountServerName":serverName
-    }
-    postData = urllib.urlencode(data)
-    url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertRegister"
-    #url = "http://192.168.0.120:8080/experts/service/ExpertRegister"
-    req = urllib2.Request(url, postData)
-    response = urllib2.urlopen(req)
-    content = response.read()
-    return  str(json.loads(content))
+    try:
+        data = {
+            "ExpertCode":expertCode,
+            "AccountLoginId":loginId,
+            "AccountCompanyName":companyName,
+            "AccountServerName":serverName
+        }
+        postData = urllib.urlencode(data)
+        url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertRegister"
+        #url = "http://192.168.0.120:8080/experts/service/ExpertRegister"
+        req = urllib2.Request(url, postData)
+        response = urllib2.urlopen(req)
+        content = response.read()
+        return  str(json.loads(content))
+    except:
+        return u"{'errcode':-1,'errmsg':'fail to connect server'}"
+    
     
 
 
