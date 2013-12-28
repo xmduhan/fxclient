c1708ebd6245b09af65de81ba109c896b879fc22
xmduhan <xmduhan@gmail.com>
2013-12-28 11:54:39 +0800

    ...

--- mt4lib/service.py
+++ mt4lib/service.py
@@ -7,6 +7,17 @@ import urllib,urllib2,cookielib,json
 #opener = urllib2.build_opener(ckproc)
 #urllib2.install_opener(opener)
 
+try:
+    from deveconfig import development
+except:
+    development = False
+
+if development :
+    serviceRoot = "http://192.168.0.120:8080"    
+else:
+    serviceRoot = "http://qisite.jios.org:11000/fxserver"   
+
+
 def expertRegister(expertCode,loginId,companyName,serverName):
     try:
         data = {
@@ -16,8 +27,9 @@ def expertRegister(expertCode,loginId,companyName,serverName):
             "AccountServerName":serverName
         }
         postData = urllib.urlencode(data)
-        url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertRegister"
+        #url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertRegister"
         #url = "http://192.168.0.120:8080/experts/service/ExpertRegister"
+        url = serviceRoot + "/experts/service/ExpertRegister"
         req = urllib2.Request(url, postData)
         response = urllib2.urlopen(req)
         content = response.read()
@@ -34,8 +46,9 @@ def expertUnregister(expertInstanceId,token):
             "Token":token
         }
         postData = urllib.urlencode(data)
-        url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertUnregister"
+        #url = "http://qisite.jios.org:11000/fxserver/experts/service/ExpertUnregister"
         #url = "http://192.168.0.120:8080/experts/service/ExpertUnregister"
+        url = serviceRoot + "/experts/service/ExpertUnregister"
         req = urllib2.Request(url, postData)
         response = urllib2.urlopen(req)
         content = response.read()
